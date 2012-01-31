2c67a6d5cf4f796ee3cdc4718218f9ae0fb96ef8
Matt Hackett <richtaur@gmail.com>
2012-01-30 22:33:28 -0800

    extra catch for gain presence and exports version

--- audia.js
+++ audia.js
@@ -3,6 +3,7 @@
 // TODO: fade methods?
 // TODO: implement proper looping
 // TODO: repeating via start/end points
+// TODO: if muteAll has been set, any newly created objects should also be muted
 
 var Audia = (function () {
 	var supported = true;
@@ -238,12 +239,18 @@ var Audia = (function () {
 
 	Audia.prototype.mute = function () {
 		this._muted = true;
-		this._gain.gain.value = 0;
+
+		if (this._gain) {
+			this._gain.gain.value = 0;
+		}
 	};
 
 	Audia.prototype.unmute = function () {
 		this._muted = false;
-		this._gain.gain.value = this._volume;
+
+		if (this._gain) {
+			this._gain.gain.value = this._volume;
+		}
 	};
 
 	Audia.prototype._regenerateBuffer = function () {
